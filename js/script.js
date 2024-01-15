1e11d9e70942bb2f2abfb769a6697e42593909ca
Metone1 <evgeny.chernov.one@gmail.com>
2024-01-15 11:26:46 +0300

    initialization

---
+++ js/script.js
@@ -0,0 +1,63 @@
+jQuery(($) => {
+      $('.select').on('click', '.select__head', function () {
+          if ($(this).hasClass('open')) {
+              $(this).removeClass('open');
+              $(this).next().fadeOut();
+          } else {
+              $('.select__head').removeClass('open');
+              $('.select__list').fadeOut();
+              $(this).addClass('open');
+              $(this).next().fadeIn();
+          }
+      });
+  
+      $('.select').on('click', '.select__item', function () {
+          $('.select__head').removeClass('open');
+          $(this).parent().fadeOut();
+          $(this).parent().prev().text($(this).text());
+          $(this).parent().prev().prev().val($(this).text());
+      });
+  
+      $(document).click(function (e) {
+          if (!$(e.target).closest('.select').length) {
+              $('.select__head').removeClass('open');
+              $('.select__list').fadeOut();
+          }
+      });
+  });
+
+const burgerMenu = document.querySelector('.burger');
+const checkbox = document.querySelector('.burger__checkbox');
+checkbox.addEventListener('click', function () {
+   if (checkbox.checked == true) {
+      burgerMenu.classList.add('active');
+      document.body.classList.add('lock');
+   }
+   else {
+      burgerMenu.classList.remove('active');
+      document.body.classList.remove('lock');
+   }
+});
+
+const preferencesMenu = document.querySelector('.preferences__menu');
+const preferencesCheckbox = document.querySelector('.preferences__checkbox');
+preferencesCheckbox.addEventListener('click', function () {
+   if (preferencesCheckbox.checked == true) {
+      preferencesMenu.classList.add('active');
+      preferencesCheckbox.classList.add('active');
+      document.body.classList.add('lock');
+   }
+   else {
+      preferencesMenu.classList.remove('active');
+      preferencesCheckbox.classList.remove('active');
+      document.body.classList.remove('lock');
+   }
+});
+
+
+const searchBtn = document.querySelector('#mobile-first__search');
+const searchMobile = document.querySelector('#mobile__search');
+searchBtn.addEventListener('click', function (){
+    searchBtn.classList.toggle('active')
+    searchMobile.classList.toggle('active')
+})
